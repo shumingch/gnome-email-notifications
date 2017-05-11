3087f7873a2a67777ce1dcffdbdda86bfc137251
Shuming Chan <shuming0207@gmail.com>
2017-05-11 11:55:40 -0400

    Cleaned up rexml.js

--- rexml.js
+++ rexml.js
@@ -10,81 +10,81 @@ function REXML(XML) {
     this.rootElement = null;
 
     this.parse = REXML_parse;
-    if (this.XML && this.XML != "") this.parse();
+    if (this.XML && this.XML !== "") this.parse();
 }
 
 function REXML_parse() {
-    var reTag = new RegExp("<([^>/ ]*)([^>]*)>", "g"); // matches that tag name $1 and attribute string $2
-    var reTagText = new RegExp("<([^>/ ]*)([^>]*)>([^<]*)", "g"); // matches tag name $1, attribute string $2, and text $3
-    var strType = "";
-    var strTag = "";
-    var strText = "";
-    var strAttributes = "";
-    var strOpen = "";
-    var strClose = "";
-    var iElements = 0;
-    var xmleLastElement = null;
-    if (this.XML.length == 0) return;
-    var arrElementsUnparsed = this.XML.match(reTag);
-    var arrElementsUnparsedText = this.XML.match(reTagText);
-    var i = 0;
-    if (arrElementsUnparsed[0].replace(reTag, "$1") == "?xml") i++;
+    const reTag = new RegExp("<([^>/ ]*)([^>]*)>", "g"); // matches that tag name $1 and attribute string $2
+    const reTagText = new RegExp("<([^>/ ]*)([^>]*)>([^<]*)", "g"); // matches tag name $1, attribute string $2, and text $3
+    let strType = "";
+    let strTag = "";
+    let strText = "";
+    let strAttributes = "";
+    let strOpen = "";
+    let strClose = "";
+    let iElements = 0;
+    let xmleLastElement = null;
+    if (this.XML.length === 0) return;
+    const arrElementsUnparsed = this.XML.match(reTag);
+    const arrElementsUnparsedText = this.XML.match(reTagText);
+    let i = 0;
+    if (arrElementsUnparsed[0].replace(reTag, "$1") === "?xml") i++;
 
     for (; i < arrElementsUnparsed.length; i++) {
         strTag = arrElementsUnparsed[i].replace(reTag, "$1");
         strAttributes = arrElementsUnparsed[i].replace(reTag, "$2");
         strText = arrElementsUnparsedText[i].replace(reTagText, "$3").replace(/[\r\n\t ]+/g, " "); // remove white space
         strClose = "";
-        if (strTag.indexOf("![CDATA[") == 0) {
+        if (strTag.indexOf("![CDATA[") === 0) {
             strOpen = "<![CDATA[";
             strClose = "]]>";
             strType = "cdata";
-        } else if (strTag.indexOf("!--") == 0) {
+        } else if (strTag.indexOf("!--") === 0) {
             strOpen = "<!--";
             strClose = "-->";
             strType = "comment";
-        } else if (strTag.indexOf("?") == 0) {
+        } else if (strTag.indexOf("?") === 0) {
             strOpen = "<?";
             strClose = "?>";
             strType = "pi";
         } else strType = "element";
-        if (strClose != "") {
+        if (strClose !== "") {
             strText = "";
             if (arrElementsUnparsedText[i].indexOf(strClose) > -1) strText = arrElementsUnparsedText[i];
             else {
-                for (; i < arrElementsUnparsed.length && arrElementsUnparsedText[i].indexOf(strClose) == -1; i++) {
+                for (; i < arrElementsUnparsed.length && arrElementsUnparsedText[i].indexOf(strClose) === -1; i++) {
                     strText += arrElementsUnparsedText[i];
                 }
                 strText += arrElementsUnparsedText[i];
             }
-            if (strText.substring(strOpen.length, strText.indexOf(strClose)) != "") {
+            if (strText.substring(strOpen.length, strText.indexOf(strClose)) !== "") {
                 xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement(strType, "", "", xmleLastElement, strText.substring(strOpen.length, strText.indexOf(strClose)));
-                if (strType == "cdata") xmleLastElement.text += strText.substring(strOpen.length, strText.indexOf(strClose));
+                if (strType === "cdata") xmleLastElement.text += strText.substring(strOpen.length, strText.indexOf(strClose));
             }
             if (strText.indexOf(strClose) + strClose.length < strText.length) {
                 xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText.substring(strText.indexOf(strClose) + strClose.length, strText.length));
-                if (strType == "cdata") xmleLastElement.text += strText.substring(strText.indexOf(strClose) + strClose.length, strText.length);
+                if (strType === "cdata") xmleLastElement.text += strText.substring(strText.indexOf(strClose) + strClose.length, strText.length);
             }
             continue;
         }
-        if (strText.replace(/ */, "") == "") strText = "";
-        if (arrElementsUnparsed[i].substring(1, 2) != "/") {
-            if (iElements == 0) {
+        if (strText.replace(/ */, "") === "") strText = "";
+        if (arrElementsUnparsed[i].substring(1, 2) !== "/") {
+            if (iElements === 0) {
                 xmleLastElement = this.rootElement = new REXML_XMLElement(strType, strTag, strAttributes, null, strText);
                 iElements++;
-                if (strText != "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
-            } else if (arrElementsUnparsed[i].substring(arrElementsUnparsed[i].length - 2, arrElementsUnparsed[i].length - 1) != "/") {
+                if (strText !== "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
+            } else if (arrElementsUnparsed[i].substring(arrElementsUnparsed[i].length - 2, arrElementsUnparsed[i].length - 1) !== "/") {
                 xmleLastElement = xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement(strType, strTag, strAttributes, xmleLastElement, strText);
                 iElements++;
-                if (strText != "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
+                if (strText !== "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
             } else {
                 xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement(strType, strTag, strAttributes, xmleLastElement, strText);
-                if (strText != "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
+                if (strText !== "") xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
             }
         } else {
             xmleLastElement = xmleLastElement.parentElement;
             iElements--;
-            if (xmleLastElement && strText != "") {
+            if (xmleLastElement && "" !== strText) {
                 xmleLastElement.text += strText;
                 xmleLastElement.childElements[xmleLastElement.childElements.length] = new REXML_XMLElement("text", "", "", xmleLastElement, strText);
             }
@@ -97,7 +97,7 @@ function REXML_XMLElement(strType, strName, strAttributes, xmlParent, strText) {
     this.name = strName;
     this.attributeString = strAttributes;
     this.attributes = null;
-    this.childElements = new Array();
+    this.childElements = [];
     this.parentElement = xmlParent;
     this.text = strText; // text of element
 
@@ -106,45 +106,56 @@ function REXML_XMLElement(strType, strName, strAttributes, xmlParent, strText) {
     this.attribute = REXML_XMLElement_attribute;
 }
 
+/**
+ * @return {string}
+ */
 function REXML_XMLElement_getText() {
-    if (this.type == "text" || this.type == "cdata") {
+    if (this.type === "text" || this.type === "cdata") {
         return this.text;
     } else if (this.childElements.length) {
-        var L = "";
-        for (var i = 0; i < this.childElements.length; i++) {
+        let L = "";
+        for (let i = 0; i < this.childElements.length; i++) {
             L += this.childElements[i].getText();
         }
         return L;
     } else return "";
 }
 
+/**
+ * @return {*}
+ */
 function REXML_XMLElement_childElement(strElementName) {
-    for (var i = 0; i < this.childElements.length; i++) if (this.childElements[i].name == strElementName) return this.childElements[i];
+    for (let i = 0; i < this.childElements.length; i++) if (this.childElements[i].name === strElementName) return this.childElements[i];
     return null;
 }
 
+/**
+ * @return {string}
+ */
 function REXML_XMLElement_attribute(strAttributeName) {
     if (!this.attributes) {
-        var reAttributes = new RegExp(" ([^= ]*)=", "g"); // matches attributes
+        let reAttributes = new RegExp(" ([^= ]*)=", "g"); // matches attributes
         if (this.attributeString.match(reAttributes) && this.attributeString.match(reAttributes).length) {
-            var arrAttributes = this.attributeString.match(reAttributes);
+            let arrAttributes = this.attributeString.match(reAttributes);
             if (!arrAttributes.length) arrAttributes = null;
-            else for (var j = 0; j < arrAttributes.length; j++) {
-                arrAttributes[j] = new Array(
-                    (arrAttributes[j] + "").replace(/[= ]/g, ""),
-                    ParseAttribute(this.attributeString, (arrAttributes[j] + "").replace(/[= ]/g, ""))
-                );
+            else for (let j = 0; j < arrAttributes.length; j++) {
+                arrAttributes[j] = [(arrAttributes[j] + "").replace(/[= ]/g, ""),
+                    ParseAttribute(this.attributeString, (arrAttributes[j] + "").replace(/[= ]/g, ""))];
             }
             this.attributes = arrAttributes;
         }
     }
-    if (this.attributes) for (var i = 0; i < this.attributes.length; i++) if (this.attributes[i][0] == strAttributeName) return this.attributes[i][1];
+    if (this.attributes) for (let i = 0; i < this.attributes.length; i++) if (this.attributes[i][0] === strAttributeName) return this.attributes[i][1];
     return "";
 }
 
+/**
+ * @return {string}
+ */
 function ParseAttribute(str_input, Attribute) {
-    var str = str_input + ">";
-    if (str.indexOf(Attribute + "='") > -1) var Attr = new RegExp(".*" + Attribute + "='([^']*)'.*>");
-    else if (str.indexOf(Attribute + '="') > -1) var Attr = new RegExp(".*" + Attribute + '="([^"]*)".*>');
+    const str = str_input + ">";
+    let Attr;
+    if (str.indexOf(Attribute + "='") > -1) Attr = new RegExp(".*" + Attribute + "='([^']*)'.*>");
+    else if (str.indexOf(Attribute + '="') > -1) Attr = new RegExp(".*" + Attribute + '="([^"]*)".*>');
     return str.replace(Attr, "$1");
 }
