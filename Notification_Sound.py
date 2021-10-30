c203626265982a96cda33b934a7780fc0314daba
Sai Suman Chitturi <56757878+ChitturiSaiSuman@users.noreply.github.com>
2021-10-30 20:22:40 +0530

    Rectified a bug in relative path

--- Notification_Sound.py
+++ Notification_Sound.py
@@ -5,11 +5,11 @@
 
 from subprocess import run
 
-extension_location = '~/.local/share/gnome-shell/extensions/GmailMessageTray@shuming0207.gmail.com'
+extension_location = '~/.local/share/gnome-shell/extensions/GmailMessageTray@shuming0207.gmail.com/Sound.ogg'
 # Default location for extension when installed
 
 # Command line audio player for playing Notification sound
 # Feel free to use 'cvlc' or 'mpg321'
 player = 'paplay'
 
-run([player, extension_location + 'Sound.py'])
+run([player, extension_location])
