4531655a3fa621bd88ff37821d7186b529af978b
Shuming Chan <shuming0207@gmail.com>
2026-02-15 10:15:51 -0800

    Restore copyright headers missing in ESM migration

--- NotificationFactory.js
+++ NotificationFactory.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2012-2017 Gnome Email Notifications contributors
+ *
+ * Gnome Email Notifications Extension is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * Gnome Email Notifications Extension is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Gnome Documents; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as MsgTray from 'resource:///org/gnome/shell/ui/messageTray.js';
 import Gio from 'gi://Gio';
